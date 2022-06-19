ab2101efbdb3140646786a5c8d99e51ac97eadef
chandana105 <chandanamj325@gmail.com>
2022-06-19 20:06:08 +0530

    fix : font issues

--- mobile/components/AppScreens/screen1.js
+++ mobile/components/AppScreens/screen1.js
@@ -1,83 +1,81 @@
-import React from "react";
+import React from 'react';
 import {
   Text,
-  View,
+
   Box,
-  Heading,
+
   FlatList,
   HStack,
-  Avatar,
+
   VStack,
   Spacer,
-  SunIcon,
-  Pressable,
+
   Button,
   Progress,
-} from "native-base";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Fontisto } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
+} from 'native-base';
+import {
+  MaterialCommunityIcons, MaterialIcons, Fontisto, AntDesign,
+} from '@expo/vector-icons';
 
-const Screen1 = () => {
+function Screen1() {
   const data = [
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-      fullName: "Change password",
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      fullName: 'Change password',
       Icon: (
         <MaterialCommunityIcons
           name="form-textbox-password"
-          size={40}
-          color="black"
+          size={35}
+          color="#6b7280"
           p="6"
         />
       ),
     },
     {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-      fullName: "General",
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      fullName: 'General',
       Icon: (
-        <MaterialIcons name="account-balance" size={40} color="black" p="6" />
+        <MaterialIcons name="account-balance" size={35} color="#6b7280" p="6" />
       ),
     },
     {
-      id: "58694a0f-3da1-471f-bd96-145571e29d72",
-      fullName: "Manage Accounts",
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      fullName: 'Manage Accounts',
       Icon: (
         <MaterialCommunityIcons
           name="account-cog"
-          size={40}
-          color="black"
+          size={35}
+          color="#6b7280"
           p="6"
         />
       ),
     },
     {
-      id: "68694a0f-3da1-431f-bd56-142371e29d72",
-      fullName: "Language",
-      language: "English",
-      Icon: <Fontisto name="language" size={40} p="6" color="black" />,
+      id: '68694a0f-3da1-431f-bd56-142371e29d72',
+      fullName: 'Language',
+      language: 'English',
+      Icon: <Fontisto name="language" size={35} p="6" color="#6b7280" />,
     },
     {
-      id: "28694a0f-3da1-471f-bd96-142456e29d72",
-      fullName: "Linked Accounts",
+      id: '28694a0f-3da1-471f-bd96-142456e29d72',
+      fullName: 'Linked Accounts',
       Icon: (
         <MaterialCommunityIcons
           name="target-account"
-          size={40}
-          color="black"
+          size={35}
+          color="#6b7280"
           p="6"
         />
       ),
     },
     {
-      id: "28694a0f-3da1-471f-bd96-142456e29d72",
-      fullName: "Disable Accounts",
+      id: '28694a0f-3da1-471f-bd96-142456e29d72',
+      fullName: 'Disable Accounts',
       Icon: (
         <MaterialCommunityIcons
           name="account-off-outline"
-          size={40}
-          color="black"
+          size={35}
+          color="#6b7280"
           p="6"
         />
       ),
@@ -88,9 +86,7 @@ const Screen1 = () => {
       bg="violet.50"
       height="full"
       alignContent="center"
-      fontFamily="SF Pro"
-      fontWeight={500}
-     
+
     >
       <Box bg="white" p="5">
         <FlatList
@@ -98,35 +94,26 @@ const Screen1 = () => {
           renderItem={({ item }) => (
             <Box
               _dark={{
-                borderColor: "gray.600",
+                borderColor: 'gray.600',
               }}
               pr="5"
               py="2"
             >
-              <HStack space={3} justifyContent="space-between">
+              <HStack space={3} justifyContent="space-between" alignItems="center">
                 {item.Icon}
 
                 <VStack p="3">
                   <Text
-                    _dark={{
-                      color: "warmGray.50",
-                    }}
+                    fontSize="md"
                     color="coolGray.800"
-                    bold
-                    alignSelf="center"
-                    
                   >
                     {item.fullName}
                   </Text>
                 </VStack>
                 <Spacer />
                 <Text
-                  fontSize="xs"
-                  _dark={{
-                    color: "warmGray.50",
-                  }}
-                  color="coolGray.800"
-                  alignSelf="center"
+                  fontSize="md"
+                  color="coolGray.500"
                 >
                   {item.language}
                 </Text>
@@ -156,10 +143,10 @@ const Screen1 = () => {
           <Box justifyContent="space-between" flex={0.95}>
             <VStack space="2">
               <HStack space="3">
-                <AntDesign name="cloudo" size={27} color="black" />
+                <AntDesign name="cloudo" size={27} color="coolGray.500" />
                 <Text
                   _dark={{
-                    color: "warmGray.50",
+                    color: 'warmGray.50',
                   }}
                   color="coolGray.800"
                   bold
@@ -172,7 +159,7 @@ const Screen1 = () => {
               <Progress
                 bg="coolGray.100"
                 _filledTrack={{
-                  bg: "violet.900",
+                  bg: 'violet.900',
                 }}
                 size="md"
                 mb={3}
@@ -183,22 +170,20 @@ const Screen1 = () => {
               textTransform="uppercase"
               fontSize="sm"
               fontWeight="bold"
-              color="black"
+              color="coolGray.800"
             >
               4 gb of 15gb used
             </Text>
           </Box>
           <Button
             size="md"
-            // width={120}
-            // height={50}
             mt="auto"
             mb="auto"
             variant="outline"
             colorScheme="secondary"
-            borderColor="violet.900"
+            borderColor="violet.800"
             _text={{
-              color: "violet.900",
+              color: 'violet.800',
             }}
           >
             Buy Storage
@@ -207,6 +192,6 @@ const Screen1 = () => {
       </Box>
     </Box>
   );
-};
+}
 
 export default Screen1;
